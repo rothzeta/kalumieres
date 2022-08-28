899723f703bf713a669f138c9761c55a0a07f725
Rothzeta <cedric.kalu@gmail.com>
2022-08-28 00:26:01 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Title: Ka Lumières
+# Version: 1.0
+# Description: Ka Lumières est une entreprise spécialisée dans le domaine juridique et dans la rédaction de texte de tout type
+# License: *TODO*
---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    class="bg-balance d-flex flex-column flex-nowrap justify-content-center justify-content-md-start align-items-center">
+    <slot />
+  </div>
+</template>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Merriweather&display=swap');
+
+/** set bootstrap font variable to merriweather */
+body {
+  --bs-font-sans-serif:  Merriweather, sans-serif;
+}
+
+.bg-balance::before {
+  content: "";
+
+  position: fixed;
+  left: 0;
+  right: 0;
+
+  width: 100%;
+  height: 100%;
+
+  z-index: -1;
+  display: block;
+  background: border-box no-repeat fixed;
+  background-position: center;
+  background-size: cover;
+  background-image: url('~/assets/img/ka-lumieres-bg.jpeg');
+  filter:  blur(1px) brightness(0.8);
+}
+
+.bg-balance {
+  height: 100vh;
+  background: transparent;
+  position: relative;
+}
+
+.text-yellow-gold-gradient {
+  background: linear-gradient(90deg, rgba(255, 204, 5, 1), rgba(214, 133, 12, 1));
+  background-clip: text;
+  color: transparent;
+}
+
+</style> 
