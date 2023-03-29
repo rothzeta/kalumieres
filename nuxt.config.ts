fe1f1c78ebadda9e6a2e29a615cffb2dfd273ae0
Rothzeta <cedric.kalu@gmail.com>
2023-03-29 16:36:09 +0000

    v2.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,46 +1,48 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt/config'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    meta: {
-        title: 'Ka lumières',
-        favicon: 'favicon.ico',
-        meta: [
+    app: {
+        head: {
+            title: 'Ka lumières',
+            favicon: 'favicon.ico',
+            meta: [
 
-            // Content security policy
-            { httpEquiv: "Content-Security-Policy", content: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; media-src 'self' data:;" },
+                // Content security policy
+                { httpEquiv: "Content-Security-Policy", content: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; media-src 'self' data:;" },
 
-            { name: 'desc', content: 'Services dans le domaine juridique et dans la rédaction de texte (légal, slogan, marketing) de tout type' },
-            { name: 'lang', content: 'fr' },
-            { name: 'author', content: 'contact@rothzeta.com' },
+                { name: 'desc', content: 'Services dans le domaine juridique et dans la rédaction de texte (légal, slogan, marketing) de tout type' },
+                { name: 'lang', content: 'fr' },
+                { name: 'author', content: 'contact@rothzeta.com' },
 
-            // Robots
-            { name: 'robots', content: 'index, follow' },
-            { name: 'googlebot', content: 'index, follow' },
+                // Robots
+                { name: 'robots', content: 'index, follow' },
+                { name: 'googlebot', content: 'index, follow' },
 
-            // Generator
-            { name: 'generator', content: 'Nuxt.js' },
+                // Generator
+                { name: 'generator', content: 'Nuxt.js' },
 
-            // Rating
-            { name: 'rating', content: 'general' },
+                // Rating
+                { name: 'rating', content: 'general' },
 
-            //twitter card meta header
-            { name: 'twitter:card', content: 'summary' },
+                //twitter card meta header
+                { name: 'twitter:card', content: 'summary' },
 
-            // Open Graph / Facebook
-            { property: 'og:title', content: 'Ka lumières' },
-            { property: 'og:description', content: 'Services dans le domaine juridique et dans la rédaction de texte (légal, slogan, marketing) de tout type' },
-            { property: 'og:image', content: 'https://kalumieres.fr/images/logo.png' },
-            { property: 'og:url', content: 'https://kalumieres.fr' },
-            { property: 'og:type', content: 'website' },
-            { property: 'og:locale', content: 'fr_FR' },
-            { property: 'og:site_name', content: 'Ka lumières' },
+                // Open Graph / Facebook
+                { property: 'og:title', content: 'Ka lumières' },
+                { property: 'og:description', content: 'Services dans le domaine juridique et dans la rédaction de texte (légal, slogan, marketing) de tout type' },
+                { property: 'og:image', content: 'https://kalumieres.fr/images/logo.png' },
+                { property: 'og:url', content: 'https://kalumieres.fr' },
+                { property: 'og:type', content: 'website' },
+                { property: 'og:locale', content: 'fr_FR' },
+                { property: 'og:site_name', content: 'Ka lumières' },
 
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            { rel: 'manifest', href: '/site.webmanifest' },
-        ],
+            ],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                { rel: 'manifest', href: '/site.webmanifest' },
+            ],
+        }
     },
     css: [
         '@/assets/sass/app.scss'
@@ -49,6 +51,6 @@ export default defineNuxtConfig({
         // Simple usage
         'nuxt-purgecss',
         // With options
-       // ['nuxt-purgecss', { /* module options */ }],
-      ]
+        // ['nuxt-purgecss', { /* module options */ }],
+    ]
 })
